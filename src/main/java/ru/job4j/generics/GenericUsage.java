3fe79e275b2975ffbb8504babfd6b63c2894edfc
Inna Timonova <innatimonova200@gmail.com>
2022-10-20 18:28:45 +0300

    Generics [#4952] Introduction

---
+++ src/main/java/ru/job4j/generics/GenericUsage.java
@@ -0,0 +1,37 @@
+package ru.job4j.generics;
+
+import java.util.*;
+
+public class GenericUsage {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("first");
+        list.add("second");
+        list.add("third");
+        for (String s : list) {
+            System.out.println("Текущий элемент: " + s);
+        }
+        List<Integer> l = List.of(1, 2, 3, 4, 5);
+        new GenericUsage().printRsl(l);
+
+        List<Person> per = List.of(new Person("name", 21, new Date(913716000000L)));
+        new GenericUsage().printInfo(per);
+
+        List<Programmer> pro = List.of(new Programmer("name123", 23, new Date(913716000000L)));
+        new GenericUsage().printInfo(pro);
+    }
+
+    public void printRsl(Collection<?> col) {
+        for (Iterator<?> it = col.iterator(); it.hasNext(); ) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    public void printInfo(Collection<? extends Person> col) {
+        for (Iterator<? extends Person> it = col.iterator(); it.hasNext(); ) {
+            Person next = it.next();
+            System.out.println(next);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/generics/Person.java
@@ -0,0 +1,78 @@
+package ru.job4j.generics;
+
+import java.util.*;
+
+public class Person {
+    private String name;
+
+    private int age;
+
+    private Date birthday;
+
+    public Person(String name, int age, Date birthday) {
+        this.name = name;
+        this.age = age;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name) && Objects.equals(birthday, person.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, birthday);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", birthday=" + birthday +
+                '}';
+    }
+
+    public void printInfo(Collection<? extends Person> col) {
+        for (Iterator<? extends Person> it = col.iterator(); it.hasNext(); ) {
+            Person next = it.next();
+            System.out.println(next);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Person> per = List.of(new Person("name", 21, new Date(913716000000L)));
+        new GenericUsage().printInfo(per);
+
+        List<Programmer> pro = List.of(new Programmer("name123", 23, new Date(913716000000L)));
+        new GenericUsage().printInfo(pro);
+    }
+}
