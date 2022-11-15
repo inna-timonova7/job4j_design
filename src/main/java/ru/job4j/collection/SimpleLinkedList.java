26b4eedb3436e4732aedac7423c2fa401d4bce64
Inna Timonova <innatimonova200@gmail.com>
2022-11-15 19:18:52 +0300

    Container based on LinkedList [#159]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,86 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+
+    private int size = 0;
+
+    private int modCount = 0;
+
+    private Node<E> first;
+
+    private Node<E> last;
+
+    public SimpleLinkedList() {
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+        private Node<E> previous;
+
+        Node(Node<E> previous, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.previous = previous;
+        }
+    }
+
+    @Override
+    public void add(E value) {
+        final Node<E> lastNode = last;
+        final Node<E> newNode = new Node<>(lastNode, value, null);
+        last = newNode;
+        if (lastNode == null) {
+            first = newNode;
+        } else {
+            lastNode.next = newNode;
+        }
+        size++;
+        modCount++;
+
+    }
+
+    @Override
+    public E get(int index) {
+        int cursor = 0;
+        Node<E> currentNode = first;
+        if (index == Objects.checkIndex(index, size)) {
+            while (cursor++ != index) {
+                currentNode = currentNode.next;
+            }
+        }
+        return currentNode.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+
+            private final int expectedModCount = modCount;
+            private Node<E> current = first;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E value = current.item;
+                current = current.next;
+                return value;
+            }
+        };
+    }
+}
