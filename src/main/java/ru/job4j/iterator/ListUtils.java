3256d94390bfa4b878c11c510ca9ba4a3e951892
Inna Timonova <innatimonova200@gmail.com>
2022-11-28 15:54:51 +0300

    ListIterator [#350217] fixes

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -37,6 +37,6 @@ public class ListUtils {
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        removeIf(list, Predicate.isEqual(list.removeAll(elements)));
+        removeIf(list, elements::contains);
     }
 }
