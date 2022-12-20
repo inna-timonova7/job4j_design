5f6c5a547b93bfcddc5c27318d10c7c41b9b0433
Inna Timonova <innatimonova200@gmail.com>
2022-12-20 17:51:34 +0300

    HashMap [#1008] fixes

--- src/main/java/ru/job4j/map/SimpleMap.java
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -36,7 +36,6 @@ public class SimpleMap<K, V> implements Map<K, V> {
 
     private int indexFor(int hash) {
         return hash & (capacity - 1);
-//        return hash & (table.length - 1);
     }
 
     private void expand() {
