c99eb96fbf42e7cc060525b65aecc67f4b36cdd4
inna-timonova7 <timonovainna@icloud.com>
2023-03-07 11:29:16 +0300

    Buffered streams [#504979]

---
+++ src/main/java/ru/job4j/io/Buffered.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Buffered {
+    public static void main(String[] args) {
+        try (BufferedInputStream in = new BufferedInputStream(
+                new FileInputStream("data/newData.txt"));
+             /* символьный поток вместо байтового потока
+             BufferedReader in = new BufferedReader(new FileReader("data/newData.txt"));
+              */
+             BufferedOutputStream out = new BufferedOutputStream(
+                     new FileOutputStream("data/output.txt", true))) {
+            out.write(in.readAllBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
